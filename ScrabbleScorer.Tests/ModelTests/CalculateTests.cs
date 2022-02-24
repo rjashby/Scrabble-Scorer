8dfce99eb0aca4d18d6fb6ac75fdc31c3236c366
Ryan Ashby <rjapsu160@gmail.com>
2022-02-24 10:09:50 -0800

    add first test for FindScore, good fail
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- ScrabbleScorer.Tests/ModelTests/CalculateTests.cs
+++ ScrabbleScorer.Tests/ModelTests/CalculateTests.cs
@@ -1,13 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RPS;
+using ScrabbleScorer.Models;
 
 namespace ScrabbleScorer.Tests 
 {
   [TestClass]
-  public class Calculate
+  public class CalculateTests
   {
     [TestMethod]
-
+    public void FindScore_ConvertsLettersToScore_Score()
+    {
+      Calculate score = new Calculate();
+      string word = "hotdog";
+      Assert.AreEqual(17, score.FindScore(word));
+    }
     
   }
 }
--- ScrabbleScorer/Models/Calculate.cs
+++ ScrabbleScorer/Models/Calculate.cs
@@ -2,15 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ScrabbleScorer
+namespace ScrabbleScorer.Models
 {
   public class Calculate
   {
-    public int Score { get; set; }
-
     public int FindScore(string input)
     {
-      
+      return 0;
     }
   }
 }
