e7b25b3870e462d1afa14026af4f2dbbeac05959
Ryan Ashby <rjapsu160@gmail.com>
2022-02-24 09:56:38 -0800

    add calculate class and FindScore method for testing
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- ScrabbleScorer/Models/Calculate.cs
+++ ScrabbleScorer/Models/Calculate.cs
@@ -6,6 +6,45 @@ namespace ScrabbleScorer
 {
   public class Calculate
   {
-    
+    public int Score { get; set; }
+
+    public int FindScore(string input)
+    {
+      int score = 0;
+      string input = "birthday";
+      char[] letterArray = input.ToUpper().ToArray();
+    //   foreach(char letter in letterArray)
+    //   {
+    //     if(letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'L' || letter == 'N' || letter == 'R' || letter == 'S' || letter == 'T')
+    //     {
+    //       score += 1;
+    //     }
+    //     else if (letter == 'D' || letter == 'G') 
+    //     {
+    //       score += 2;
+    //     } 
+    //     else if (letter == 'B' || letter == 'C' || letter == 'M' || letter == 'P') 
+    //     {
+    //       score += 3;
+    //     } 
+    //     else if (letter == 'F' || letter == 'H' || letter == 'V' || letter == 'W' || letter == 'Y') 
+    //     {
+    //       score += 4;
+    //     } 
+    //     else if (letter == 'K' ) 
+    //     {
+    //       score += 5;
+    //     } 
+    //     else if (letter == 'J' || letter == 'X') 
+    //     {
+    //       score += 8;
+    //     } 
+    //     else if (letter == 'Q' || letter == 'Z') 
+    //     {
+    //       score += 10;
+    //     }
+    //   }
+    // return score;
+    }
   }
 }
