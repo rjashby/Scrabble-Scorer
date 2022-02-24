ac9f8b0e0ed30792dff1a28f7372b76370af30b5
Ryan Ashby <rjapsu160@gmail.com>
2022-02-24 13:00:47 -0800

    add code for if/else FindScore method
    Co-authored-by: Anastasiia Riabets <anastasiia.ria@gmail.com>

--- ScrabbleScorer/Models/Calculate.cs
+++ ScrabbleScorer/Models/Calculate.cs
@@ -8,13 +8,60 @@ namespace ScrabbleScorer.Models
   {
     public int FindScore(string input)
     {
-      return 0;
+      int score = 0;
+      string input = "birthday";
+      char[] letterArray = input.ToUpper().ToArray();
+      foreach(char letter in letterArray)
+      {
+        if(letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U' || letter == 'L' || letter == 'N' || letter == 'R' || letter == 'S' || letter == 'T')
+        {
+          score += 1;
+        }
+        else if (letter == 'D' || letter == 'G') 
+        {
+          score += 2;
+        } 
+        else if (letter == 'B' || letter == 'C' || letter == 'M' || letter == 'P') 
+        {
+          score += 3;
+        } 
+        else if (letter == 'F' || letter == 'H' || letter == 'V' || letter == 'W' || letter == 'Y') 
+        {
+          score += 4;
+        } 
+        else if (letter == 'K' ) 
+        {
+          score += 5;
+        } 
+        else if (letter == 'J' || letter == 'X') 
+        {
+          score += 8;
+        } 
+        else if (letter == 'Q' || letter == 'Z') 
+        {
+          score += 10;
+        }
+      }
+    return score;
     }
 
     public string[] GetArray(string input)
     {
       string[] array = new string[1] {input};
       return array;
+    }
+
+    public static int CheckScore(string input)
+    {
+      int OnePointSquares = Regex.Matches(input, "[aeioulnrst]", RegexOptions.IgnoreCase).Count;
+      int TwoPointSquares = Regex.Matches(input, "[qg]", RegexOptions.IgnoreCase).Count * 2;
+      int ThreePointSquares =  Regex.Matches(input, "[bcmp]", RegexOptions.IgnoreCase).Count * 3;
+      int FourPointSquares =  Regex.Matches(input, "[fhvwy]", RegexOptions.IgnoreCase).Count * 4;
+      int FivePointSquares =  Regex.Matches(input, "[k]", RegexOptions.IgnoreCase).Count * 5;
+      int EightPointSquares =  Regex.Matches(input, "[jx]", RegexOptions.IgnoreCase).Count * 8;
+      int TenPointSquares =  Regex.Matches(input, "[qz]", RegexOptions.IgnoreCase).Count * 10;
+      int TotalPoints = OnePointSquares + TwoPointSquares + ThreePointSquares + FourPointSquares + FivePointSquares + EightPointSquares + TenPointSquares;
+      return TotalPoints;
     }
   }
 }
